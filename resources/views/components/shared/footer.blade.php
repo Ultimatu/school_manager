99af2c145196055c3d762949271362c4e37ed251
Ultimatu <tondesoulco@gmail.com>
2024-02-19 10:35:58 +0000

    update

---
+++ resources/views/components/shared/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-dark">
+    <div class="container-fluid p-4 px-sm-5">
+        <div class="d-flex flex-column flex-lg-row gap-3 text-white">
+            <span class="opacity-50">Copyright &copy; 2023. Dashbyte. All Rights Reserved.</span>
+        </div>
+    </div><!-- container-fluid -->
+</footer>

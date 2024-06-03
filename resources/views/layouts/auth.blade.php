93803e436c3795440e88fc8891b3887ebafdf865
Ultimatu <tondesoulco@gmail.com>
2024-06-03 19:10:14 +0000

    update

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,7 +18,6 @@
       <!-- notification -->
       <div id="notification"></div>
       <!-- end notification -->
-      {{ $slot }}
     @yield('content')
     <script src="{{ asset('lib/jquery/jquery.min.js') }}"></script>
     <script src="{{ asset("lib/jquery-validation/jquery.validate.min.js") }}"></script>
